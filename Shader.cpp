0653f48e0644d3201a3fe976fe8ae476d9d4f633
dan olson <dano7900@gmail.com>
2020-09-13 15:00:33 -0400

    update shader

--- Shader.cpp
+++ Shader.cpp
@@ -58,15 +58,53 @@ Shader::Shader(const char* vertPath,const char* fragPath) {
     glDeleteShader(vert);
     glDeletShader(frag);
 
+}
+
+void use() {
+
+    glUseProgram(id);
+}
+
+void setBool(const std::string &name,bool value) const {
+
+    glUniform1i(glGetUniformLocation(id,name.c_str()),(int)value);
+}
+
+void setInt(const std::string &name,int value) const {
+
+    glUniform1i(glGetUniformLocation(id,name.c_str()),value);
+}
+
+void setFloat(const std::string &name,int value) const {
+
+   glUniform1f(glGetUniformLocation(id,name.c_str()),value);
+}
+
+void checkCompileErrors(unsigned int shader,std::string type) {
+
+    int success;
+    char infoLog[1024];
+  
+    if(type != "PROGRAM") {
+
+       glGetShaderiv(shader,GL_COMPILE_STATUS,&success);  
+
+        if(!success) {
+            glGetShaderInfoLog(shader,1024,NULL,infoLog);
+            std::cout <<  "ERROR::type: "
+            << type << "\n" << infoLog << std::endl; 
+
+        }
+
+    } else {
+       
+        glGetProgramiv(shader,GL_LINK_STATUS,&success);
+
+        if(!success) {
+            glGetProgramInfoLog(shader,1024,NULL,infoLog);
+            std::cout << "ERROR::type: "
+            << type << "\n" << infoLog << std::endl;
+
+        }
     }
-
-
-
-
-
-
-
-
-   
-
-
+}
