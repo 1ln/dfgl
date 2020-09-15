d37910333598c865e9dd94f3282c9d8120dee1b2
dan olson <dano7900@gmail.com>
2020-09-15 12:45:16 -0400

    update

--- Camera.h
+++ Camera.h
@@ -27,12 +27,7 @@ public:
     Camera(glm::vec3 pos = glm::vec3(0.0f,0.0f,0.0f),
            glm::vec3 up  = glm::vec3(0.0f,1.0f,0.0f),
            float yawd    = YAW,
-           float pitchd  = PITCH):
-
-           front(glm::vec3(0.0f,0.0f,-1.0f)),
-           move_speed(SPEED),
-           mouse_sensitivity(SENSITIVITY),
-           zoom(ZOOM)
+           float pitchd  = PITCH);
                     
     glm::mat4 getViewMatrix();
 
