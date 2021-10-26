4422954d05f3a7c9e370dac930d1758e072fbb6d
dan olson <dano7900@gmail.com>
2021-10-26 16:52:56 -0400

    update vert

--- render_vert.glsl
+++ render_vert.glsl
@@ -1,27 +1,12 @@
-#version 430 core
+#version 330 core
 
-uniform mat4 view;
-uniform mat4 projection;
-uniform mat4 model;
+layout (location = 0) in vec2 pos;
+layout (location = 1) in vec2 texCoords;
 
-layout (location = 0) out vec2 uv;
-layout (location = 0) in vec3 pos;
+out vec2 texc;
 
 void main() {
 
-#ifdef ATTTRIBUTELESS
-
-float x = float(((uint(gl_VertexID) + 2u) / 3u) % 2u );
-float y = float(((uint(gl_VertexID) + 2u) / 3u) % 2u );
-
-gl_Position = vec4(-1. + x*2.,-1. + y*2.,1.,0.);
-uv = vec2(x,y);
-
-#else
-
-//gl_Position = model * view * projection * vec4(pos,1.);
-gl_Position = vec4(pos,1.);
-
-#endif
-
+texc = texCoords;
+gl_Position = vec4(pos.x,pos.y,0.,1.);
 }
