6405ec64b5ff92807bc8245c93270b30fac89267
dan olson <dano7900@gmail.com>
2021-10-26 17:30:46 -0400

    update

--- render.glsl
+++ render.glsl
@@ -1,27 +1,12 @@
-#version 430 core
+#version 330 core
 
-uniform mat4 view;
-uniform mat4 projection;
-uniform mat4 model;
+out vec4 FragColor;
+in vec2 texc;
 
-layout (location = 0) out vec2 uv;
-layout (location = 0) in vec3 pos;
+uniform sampler2D tex1;
 
 void main() {
 
-#ifdef ATTTRIBUTELESS
-
-float x = float(((uint(gl_VertexID) + 2u) / 3u) % 2u );
-float y = float(((uint(gl_VertexID) + 2u) / 3u) % 2u );
-
-gl_Position = vec4(-1. + x*2.,-1. + y*2.,1.,0.);
-uv = vec2(x,y);
-
-#else
-
-//gl_Position = model * view * projection * vec4(pos,1.);
-gl_Position = vec4(pos,1.);
-
-#endif
+FragColor = texture(tex1,texc);
 
 }
