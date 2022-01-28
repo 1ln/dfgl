22bd932038e7bf8306e362457dd8df7d24ce4f8a
dan olson <dano7900@gmail.com>
2022-01-27 20:27:41 -0500

    update

--- shaders/render.glsl
+++ shaders/render.glsl
@@ -993,12 +993,11 @@ p = (vec4(p,1.)*mx*my).xyz;
 #endif
 
 p.xz *= rot(time*.1);
-
+p.xz *= rot(.5*easeInOut(sin(time*.5)*.25)+.125);
 
 vec3 q = p;
 res = opu(res,vec2(box(p,vec3(1.)),2.));
 res = opu(res,vec2(plane(q,vec4(0.,1.,0.,1.)),1.));
-
 
 
 
