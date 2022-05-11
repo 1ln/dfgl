5e849d32eae3379a9fda3459f01930d4622bcd8e
dan olson <dano7900@gmail.com>
2022-05-11 14:39:14 -0400

    update

--- shaders/align.glsl
+++ shaders/align.glsl
@@ -59,7 +59,8 @@ void main() {
 vec3 c = vec3(0.);
 vec3 fc = vec3(0.);
 
-float m_dist = 0.;
+float md = 1.;
+float d = 0.;
 
 for(int i = 0; i < AA; i++ ) {
    for(int k = 0; k < AA; k++) {
@@ -68,54 +69,48 @@ for(int i = 0; i < AA; i++ ) {
        vec2 uv = (2.* (FC+o) -
        RE.xy)/RE.y;
 
-       float d = cell(uv,12.,m_dist); 
-       vec2 h = fract(uv); 
-
-
-    vec2 p = floor(x);
-    vec2 f = fract(x);
+       vec2 p = floor(uv);
+       vec2 f = fract(uv);
      
-    for(int i = -1; i <= 1; i++) {
-        for(int k = -1; k <= 1; k++) {
+       for(int i = -1; i <= 1; i++) {
+           for(int k = -1; k <= 1; k++) {
 
-                vec2 b = vec2(float(k),float(i));
+            vec2 b = vec2(float(k),float(i));
 
-                vec2 r = vec2(h(p.x+b.x+h(p.y+b.y)));
-                r = .5 + .5*sin(TAU+T*r);                
+            vec2 r = vec2(h(p.x+b.x+h(p.y+b.y)));
+            r = .5 + .5*sin(TAU+T*r);                
 
-                vec2 diff = (b + r - f);
+            vec2 diff = (b + r - f);
 
-                float d = length(diff);
+            d = length(diff);
 
-                md = .5 *
-                1./
-                max(min(md,2.*d),
-                p.x-p.y);                               
+            md = .5 *
+            1./
+            max(min(md,2.*d),
+            p.x-p.y);                               
                         
         }
     }
  
+    c += vec3(d);    
+    c += 1.-step(.02,md); 
+    c.g += step(.98,f.x) + step(.98,f.y);
+    c.rg += fm(c.y,
+    vec2(.1,5.),
+    vec2(.2,.1),
+    vec2(2.,.1),
+    vec2(.5,-.5));
 
 
-       c += vec3(d);    
-       c += 1.-step(.02,m_dist); 
-       c.g += step(.98,h.x) + step(.98,h.y);
-       c.rg += fm(c.y,
-            vec2(.1,5.),
-            vec2(.2,.1),
-            vec2(2.,.1),
-            vec2(.5,-.5));
+    c = pow(c,vec3(GAMMA));
+    fc += c;
 
-
-
-       c = pow(c,vec3(GAMMA));
-       fc += c;
    }
 }   
   
-   fc /= float(AA*AA);
+    fc /= float(AA*AA);
 
-   fragColor = vec4(fc,1.0);
+    fragColor = vec4(fc,1.0);
 
 
 }
